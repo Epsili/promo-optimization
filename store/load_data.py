e3e4ed4b06fc8b2c0dafd16045fe04c295628a7b
Elie Merhej <elie@epsili.com>
2024-05-10 14:31:39 +0400

    project folder setup + test reading data

---
+++ store/load_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+dfSchedule = pd.read_csv("assignment_dataset.csv")
+dfPromos = pd.read_csv("promos.csv")
+
+
+print(dfSchedule.to_string())
+print(dfPromos.to_string())
+
+
+
+
+
